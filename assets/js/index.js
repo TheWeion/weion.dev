2a60ff925bc4c0962d523eba9e1460acdd96a034
theweion <ianterryfallows@gmail.com>
2023-12-04 11:04:33 +0000

    fix(index.html, index.html): Moved meta tags to HTML so that crawlers can detect them consistently.

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,53 +8,6 @@
 "use strict";
 
 /**
- * @function generateMetaTags
- * @description Generates meta tags for SEO.
- * @param {string} title - The title to be used for the meta tags.
- * @param {string} description - The description to be used for the meta tags.
- * @param {string} imageUrl - The image URL to be used for the meta tags.
- * @param {Array<string>} platforms - The social media platforms for which to generate the appropriate meta tags.
- * @returns {string} - The meta tags as a string.
- */
-const generateMetaTags = (title, description, imageUrl, platforms) => {
-	const titleTag = `<meta property="title" content="${title}" />`;
-	const descriptionTag = `<meta property="description" content="${description}" />`;
-
-	const platformTags = [];
-
-	for (let i = 0; i < platforms.length; i++) {
-		const platform = platforms[i];
-
-		if (platform === "facebook") {
-			platformTags.push(
-				`<meta property="og:type" content="website" />`,
-				`<meta property="og:url" content="https://weion.dev/" />`,
-				`<meta property="og:title" content="${title}" />`,
-				`<meta property="og:description" content="${description}" />`,
-				`<meta property="og:image" content="${imageUrl}" />`
-			);
-		} else if (platform === "twitter") {
-			platformTags.push(
-				`<meta name="twitter:card" content="summary_large_image" />`,
-				`<meta name="twitter:url" content="https://weion.dev/" />`,
-				`<meta name="twitter:title" content="${title}" />`,
-				`<meta name="twitter:description" content="${description}" />`,
-				`<meta name="twitter:image" content="${imageUrl}" />`
-			);
-		}
-	}
-
-	return `${titleTag}\n${descriptionTag}\n${platformTags.join("\n")}`;
-};
-
-const metaTags = generateMetaTags(
-	'Terry Fallows | Software Engineer',
-	'Welcome to my portfolio, I am a lover of all things code, currently enhancing STB UI/UX for customers at CommScope.',
-	'https://weion.dev/assets/img/meta_img.webp',
-	['facebook', 'twitter']
-);
-
-/**
  * @function disableContextMenu
  * @description Disables context menu on right-click for card IMG elements.
  */
@@ -66,9 +19,5 @@ const disableContextMenu = () => {
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-	const lastMetaTag = document.querySelector('head > meta:last-of-type');
-	if (lastMetaTag) {
-		lastMetaTag.insertAdjacentHTML('afterend', metaTags);
-	}
 	disableContextMenu();
 });
